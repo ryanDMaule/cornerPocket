1f5d4c360d1828c81e44733dcf741c230044e3e8
ryanDMaule <davidmaule98@gmail.com>
2024-05-03 14:11:26 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/cornerpocket/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.cornerpocket
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.cornerpocket.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //region NAV BAR SET UP
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.playFragment, R.id.statsFragment, R.id.historyFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        supportActionBar?.hide()
+        navView.setupWithNavController(navController)
+
+        //endregion
+
+    }
+}
