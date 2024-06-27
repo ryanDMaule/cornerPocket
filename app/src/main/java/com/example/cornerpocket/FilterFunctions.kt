294effbd10266eab19e12c752a5a262bac8a38e5
ryanDMaule <davidmaule98@gmail.com>
2024-06-27 17:29:04 +0100

    FilterFunctions class
    - Now most things related to the filter dialog have been moved into a separate class as most of this logic will be shared by 2 fragments.
    
    - Some of the classes have been slightly altered to be more generic
    
    - requires to button clicks to be handled in the specific fragments as the logic will be different for both

---
+++ app/src/main/java/com/example/cornerpocket/FilterFunctions.kt
@@ -0,0 +1,223 @@
+package com.example.cornerpocket
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AutoCompleteTextView
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.viewModelScope
+import com.example.cornerpocket.Adapters.OpponentListAdapter
+import com.example.cornerpocket.models.Game
+import com.example.cornerpocket.models.Opponent
+import com.example.cornerpocket.viewModels.FilterViewModel
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.sidesheet.SideSheetDialog
+import kotlinx.coroutines.launch
+
+class FilterFunctions {
+    companion object {
+        private var selectedOpponent : Opponent? = null
+        var btnResetFilters : ImageView? = null
+        var btnApply : MaterialButton? = null
+        fun createFiltersDialog(context: Context, li : LayoutInflater, vm : FilterViewModel) : SideSheetDialog {
+            val dialog = SideSheetDialog(context)
+            val view = li.inflate(R.layout.filters_sheet, null)
+
+            //region OPPONENT SELECT DROPDOWN
+
+            //OPPONENT DROP DOWN BUTTON
+            val actv = view.findViewById<AutoCompleteTextView>(R.id.actv)
+            vm.viewModelScope.launch {
+                vm.getOpponents().collect { opponents ->
+                    // Create and set the adapter
+                    try {
+                        val adapter = OpponentListAdapter(context, opponents.toMutableList())
+                        actv.setAdapter(adapter)
+                        actv.onItemClickListener = AdapterView.OnItemClickListener { adapterView: AdapterView<*>, _: View, i: Int, _: Long ->
+                            val selectedPerson = adapter.getItem(i) as? Opponent
+                            selectedPerson?.let {
+                                actv.setText(it.name, false) // Display only the name
+                                if (selectedPerson.name == "All") {
+                                    selectedOpponent = null
+                                } else {
+                                    // Handle the selectedPerson object
+                                    selectedPerson.let {
+                                        selectedOpponent = selectedPerson
+                                    }
+                                }
+                            }
+                        }
+                    } catch (e : Exception){
+                        //CAUSES A CRASH IF YOU FILTER RESULTS ON HISTORY PAGE GO PLAY A GAME AND CRASHES ON "RETURN TO MENU"
+                        Log.e("HF", "EXCEPTION CAUGHT : $e")
+                    }
+                }
+            }
+            //endregion
+
+            //region ON CLICK LOGIC
+
+            val btnClose = view.findViewById<ImageView>(R.id.quit_button)
+            btnClose.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            btnResetFilters = view.findViewById(R.id.clear_filters_button)
+//            btnResetFilters.setOnClickListener {
+//                //HANDLE IN SPECIFIC FRAGMENT
+//            }
+
+            //OPPONENT SELECT - FILTER
+            val opponentFilterButton = view.findViewById<ImageView>(R.id.opponentFilterAddButton)
+            val opponentFilterExpansion = view.findViewById<ConstraintLayout>(R.id.opponent_filter_expansion)
+            opponentFilterButton.setOnClickListener {
+                filterClick(opponentFilterButton, opponentFilterExpansion)
+            }
+
+            //GAME TYPE - FILTER
+            val gameTypeFilterButton = view.findViewById<ImageView>(R.id.gameTypeFilterAddButton)
+            val gameTypeFilterExpansion = view.findViewById<ConstraintLayout>(R.id.gameType_filter_expansion)
+            gameTypeFilterButton.setOnClickListener {
+                filterClick(gameTypeFilterButton, gameTypeFilterExpansion)
+            }
+
+            //RESULTS - FILTER
+            val resultsFilterButton = view.findViewById<ImageView>(R.id.results_filter_add_button)
+            val resultsFilterExpansion = view.findViewById<ConstraintLayout>(R.id.results_filter_expansion)
+            resultsFilterButton.setOnClickListener {
+                filterClick(resultsFilterButton, resultsFilterExpansion)
+            }
+
+            //BREAK - FILTER
+            val breakingFilterButton = view.findViewById<ImageView>(R.id.breaking_filter_add_button)
+            val breakingFilterExpansion = view.findViewById<ConstraintLayout>(R.id.breaking_filter_expansion)
+            breakingFilterButton.setOnClickListener {
+                filterClick(breakingFilterButton, breakingFilterExpansion)
+            }
+
+            //ORDER - FILTER
+            val orderFilterButton = view.findViewById<ImageView>(R.id.order_filter_add_button)
+            val orderFilterExpansion = view.findViewById<ConstraintLayout>(R.id.order_filter_expansion)
+            orderFilterButton.setOnClickListener {
+                filterClick(orderFilterButton, orderFilterExpansion)
+            }
+
+            //endregion
+
+            btnApply = view.findViewById(R.id.footer_button)
+//            btnApply.setOnClickListener {
+//                //HANDLE IN SPECIFIC FRAGMENT
+//            }
+
+            dialog.setContentView(view)
+            return dialog
+        }
+        private fun getSelectedRadioButtonText(radioGroup: RadioGroup): String {
+            val selectedRadioButtonId = radioGroup.checkedRadioButtonId
+            val selectedRadioButton = radioGroup.findViewById<RadioButton>(selectedRadioButtonId)
+            return selectedRadioButton.text.toString()
+        }
+        private fun getGameTypeRadioResult(selectedOption : String) : String? {
+            return when(selectedOption.uppercase()){
+                "ENGLISH" -> {
+                    "ENGLISH"
+                }
+
+                "AMERICAN" -> {
+                    "AMERICAN"
+                }
+
+                "BOTH" -> {
+                    null
+                }
+
+                else -> {
+                    null
+                }
+            }
+        }
+        private fun getUserWonRadioResult(selectedOption : String) : Boolean? {
+            return when(selectedOption.uppercase()){
+                "WINS" -> {
+                    true
+                }
+
+                "LOSSES" -> {
+                    false
+                }
+
+                "BOTH" -> {
+                    null
+                }
+
+                else -> {
+                    null
+                }
+            }
+        }
+        private fun getUserBreaksRadioResult(selectedOption : String) : Boolean? {
+            return when(selectedOption.uppercase()){
+                "ME" -> {
+                    true
+                }
+
+                "OPPONENT" -> {
+                    false
+                }
+
+                "BOTH" -> {
+                    null
+                }
+
+                else -> {
+                    null
+                }
+            }
+        }
+        private fun getOrderRadioResult(selectedOption : String) : Boolean {
+            return when(selectedOption){
+                "Date: Newest to oldest" -> {
+                    true
+                }
+
+                "Date: Oldest to newest" -> {
+                    false
+                }
+
+                else -> {
+                    true
+                }
+            }
+        }
+        private fun filterClick(button : ImageView, expansionZone : View) {
+            // CURRENTLY NOT EXPANDED '+'
+            if (expansionZone.visibility != View.VISIBLE){
+                HelperFunctions.expandView(expansionZone)
+                HelperFunctions.rotate45clockwise(button)
+            }
+            // CURRENTLY EXPANDED 'X'
+            else {
+                HelperFunctions.collapseView(expansionZone)
+                HelperFunctions.rotate45anticlockwise(button)
+            }
+        }
+        fun createFilterList(dialog : SideSheetDialog, fvm: FilterViewModel): MutableList<Game> {
+
+            return fvm.filterGames(
+                fvm.unfilteredGameList!!.toMutableList(),
+                opponent = selectedOpponent,
+                gameType = getGameTypeRadioResult(getSelectedRadioButtonText(dialog.findViewById(R.id.groupradio_gameType)!!)),
+                userWins = getUserWonRadioResult(getSelectedRadioButtonText(dialog.findViewById(R.id.groupradio_results)!!)),
+                userBreaks = getUserBreaksRadioResult(getSelectedRadioButtonText(dialog.findViewById(R.id.groupradio_breaking)!!)),
+                orderNewest = getOrderRadioResult(getSelectedRadioButtonText(dialog.findViewById(R.id.groupradio_order)!!))
+            )
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/cornerpocket/presentation/history/HistoryFragment.kt
+++ app/src/main/java/com/example/cornerpocket/presentation/history/HistoryFragment.kt
@@ -1,16 +1,9 @@
 package com.example.cornerpocket.presentation.history
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.AutoCompleteTextView
-import android.widget.ImageView
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.fragment.findNavController
@@ -18,14 +11,13 @@ import androidx.navigation.navGraphViewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cornerpocket.Adapters.GameHistoryAdapter
-import com.example.cornerpocket.Adapters.OpponentListAdapter
-import com.example.cornerpocket.HelperFunctions
+import com.example.cornerpocket.FilterFunctions
+import com.example.cornerpocket.FilterFunctions.Companion.createFiltersDialog
+import com.example.cornerpocket.FilterFunctions.Companion.createFilterList
 import com.example.cornerpocket.R
 import com.example.cornerpocket.databinding.FragmentHistoryBinding
 import com.example.cornerpocket.models.Game
-import com.example.cornerpocket.models.Opponent
 import com.example.cornerpocket.viewModels.FilterViewModel
-import com.google.android.material.button.MaterialButton
 import com.google.android.material.sidesheet.SideSheetDialog
 import kotlinx.coroutines.launch
 
@@ -37,11 +29,9 @@ class HistoryFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var historyAdapter: GameHistoryAdapter
-    private lateinit var actv : AutoCompleteTextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentHistoryBinding.inflate(inflater, container, false)
-        Log.i("HF", "onCreateView")
 
         // Inflate the layout for this fragment
         return binding.root
@@ -53,18 +43,28 @@ class HistoryFragment : Fragment() {
         // TODO: if the if is brought back in then the dialog and its filters will be retained when navigating back from the details fragment
         //  but future searches will not update the recycler view
 //        if (filterViewModel.filtersDialog == null){
-            createDialog()
+            filterViewModel.filtersDialog = createFiltersDialog(context = requireContext(), li = layoutInflater, vm = filterViewModel)
+
+            val btnResetFilters = FilterFunctions.btnResetFilters
+            btnResetFilters?.setOnClickListener {
+                resetDialogFilterLogic(filterViewModel.filtersDialog!!)
+            }
+
+            val btnApply = FilterFunctions.btnApply
+            btnApply?.setOnClickListener {
+                applyFilterLogic(filterViewModel.filtersDialog!!)
+            }
 //        }
 
         filterViewModel.viewModelScope.launch {
             filterViewModel.getGames().collect{ gamesList ->
                 filterViewModel.unfilteredGameList = gamesList
 
-                if (filterViewModel.filteredGameList != null){
-                    populateGamesAdapter(filterViewModel.filteredGameList!!)
-                } else {
+//                if (filterViewModel.filteredGameList != null){
+//                    populateGamesAdapter(filterViewModel.filteredGameList!!)
+//                } else {
                     populateGamesAdapter(gamesList)
-                }
+//                }
 
             }
         }
@@ -74,114 +74,14 @@ class HistoryFragment : Fragment() {
         }
 
     }
-
-    private fun createDialog() {
-        val dialog = SideSheetDialog(requireContext())
-        val view = layoutInflater.inflate(R.layout.filters_sheet, null)
-
-        //region OPPONENT SELECT DROPDOWN
-        var selectedOpponent : Opponent? = null
-
-        //OPPONENT DROP DOWN BUTTON
-        actv = view.findViewById(R.id.actv)
-        filterViewModel.viewModelScope.launch {
-            filterViewModel.getOpponents().collect { opponents ->
-                // Create and set the adapter
-                try {
-                    val adapter = OpponentListAdapter(requireContext(), opponents.toMutableList())
-                    actv.setAdapter(adapter)
-                    actv.onItemClickListener = AdapterView.OnItemClickListener { adapterView: AdapterView<*>, _: View, i: Int, _: Long ->
-                        val selectedPerson = adapter.getItem(i) as? Opponent
-                        selectedPerson?.let {
-                            actv.setText(it.name, false) // Display only the name
-                            if (selectedPerson.name == "All") {
-                                selectedOpponent = null
-                            } else {
-                                // Handle the selectedPerson object
-                                selectedPerson.let {
-                                    selectedOpponent = selectedPerson
-                                }
-                            }
-                        }
-                    }
-                } catch (e : Exception){
-                    //CAUSES A CRASH IF YOU FILTER RESULTS ON HISTORY PAGE GO PLAY A GAME AND CRASHES ON "RETURN TO MENU"
-                    Log.e("HF", "EXCEPTION CAUGHT : $e")
-                }
-            }
-        }
-        //endregion
-
-        //region ON CLICK LOGIC
-
-        val btnClose = view.findViewById<ImageView>(R.id.quit_button)
-        btnClose.setOnClickListener {
-            dialog.dismiss()
-        }
-
-        val btnResetFilters = view.findViewById<ImageView>(R.id.clear_filters_button)
-        btnResetFilters.setOnClickListener {
-            filterViewModel.filteredGameList = null
-            createDialog()
-            populateGamesAdapter(filterViewModel.unfilteredGameList!!)
-            dialog.dismiss()
-        }
-
-        //OPPONENT SELECT - FILTER
-        val opponentFilterButton = view.findViewById<ImageView>(R.id.opponentFilterAddButton)
-        val opponentFilterExpansion = view.findViewById<ConstraintLayout>(R.id.opponent_filter_expansion)
-        opponentFilterButton.setOnClickListener {
-            filterClick(opponentFilterButton, opponentFilterExpansion)
-        }
-
-        //GAME TYPE - FILTER
-        val gameTypeFilterButton = view.findViewById<ImageView>(R.id.gameTypeFilterAddButton)
-        val gameTypeFilterExpansion = view.findViewById<ConstraintLayout>(R.id.gameType_filter_expansion)
-        gameTypeFilterButton.setOnClickListener {
-            filterClick(gameTypeFilterButton, gameTypeFilterExpansion)
-        }
-
-        //RESULTS - FILTER
-        val resultsFilterButton = view.findViewById<ImageView>(R.id.results_filter_add_button)
-        val resultsFilterExpansion = view.findViewById<ConstraintLayout>(R.id.results_filter_expansion)
-        resultsFilterButton.setOnClickListener {
-            filterClick(resultsFilterButton, resultsFilterExpansion)
-        }
-
-        //BREAK - FILTER
-        val breakingFilterButton = view.findViewById<ImageView>(R.id.breaking_filter_add_button)
-        val breakingFilterExpansion = view.findViewById<ConstraintLayout>(R.id.breaking_filter_expansion)
-        breakingFilterButton.setOnClickListener {
-            filterClick(breakingFilterButton, breakingFilterExpansion)
-        }
-
-        //ORDER - FILTER
-        val orderFilterButton = view.findViewById<ImageView>(R.id.order_filter_add_button)
-        val orderFilterExpansion = view.findViewById<ConstraintLayout>(R.id.order_filter_expansion)
-        orderFilterButton.setOnClickListener {
-            filterClick(orderFilterButton, orderFilterExpansion)
-        }
-
-        //endregion
-
-        val btnApply = view.findViewById<MaterialButton>(R.id.footer_button)
-        btnApply.setOnClickListener {
-            applyFilterLogic(selectedOpponent, dialog, view)
-        }
-
-        dialog.setContentView(view)
-        filterViewModel.filtersDialog = dialog
+    private fun resetDialogFilterLogic(dialog : SideSheetDialog) {
+        filterViewModel.filteredGameList = null
+        filterViewModel.filtersDialog = createFiltersDialog(requireContext(), layoutInflater, filterViewModel)
+        populateGamesAdapter(filterViewModel.unfilteredGameList!!)
+        dialog.dismiss()
     }
-    private fun applyFilterLogic(selectedOpponent : Opponent?, dialog : SideSheetDialog, view : View){
-        val filteredList = filterViewModel.filterGames(
-            filterViewModel.unfilteredGameList!!.toMutableList(),
-            opponent = selectedOpponent,
-            gameType = getGameTypeRadioResult(getSelectedRadioButtonText(view.findViewById(R.id.groupradio_gameType))),
-            userWins = getUserWonRadioResult(getSelectedRadioButtonText(view.findViewById(R.id.groupradio_results))),
-            userBreaks = getUserBreaksRadioResult(getSelectedRadioButtonText(view.findViewById(R.id.groupradio_breaking))),
-            orderNewest = getOrderRadioResult(getSelectedRadioButtonText(view.findViewById(R.id.groupradio_order))))
-
-        Log.i("HF", "filteredList size : ${filteredList.size}")
+    private fun applyFilterLogic(dialog : SideSheetDialog){
+        val filteredList = createFilterList(dialog, filterViewModel)
 
         //region HISTORY ADAPTER
         populateGamesAdapter(filteredList)
@@ -204,95 +104,6 @@ class HistoryFragment : Fragment() {
                 putString("gameKey", game._id.toHexString())
             }
             findNavController().navigate(R.id.action_historyFragment_to_historyGameDetailsFragment, bundle)
-        }
-    }
-    private fun getSelectedRadioButtonText(radioGroup: RadioGroup): String {
-        val selectedRadioButtonId = radioGroup.checkedRadioButtonId
-        val selectedRadioButton = radioGroup.findViewById<RadioButton>(selectedRadioButtonId)
-        return selectedRadioButton.text.toString()
-    }
-    private fun getGameTypeRadioResult(selectedOption : String) : String? {
-        return when(selectedOption.uppercase()){
-            "ENGLISH" -> {
-                "ENGLISH"
-            }
-
-            "AMERICAN" -> {
-                "AMERICAN"
-            }
-
-            "BOTH" -> {
-                null
-            }
-
-            else -> {
-                null
-            }
-        }
-    }
-    private fun getUserWonRadioResult(selectedOption : String) : Boolean? {
-        return when(selectedOption.uppercase()){
-            "WINS" -> {
-                true
-            }
-
-            "LOSSES" -> {
-                false
-            }
-
-            "BOTH" -> {
-                null
-            }
-
-            else -> {
-                null
-            }
-        }
-    }
-    private fun getUserBreaksRadioResult(selectedOption : String) : Boolean? {
-        return when(selectedOption.uppercase()){
-            "ME" -> {
-                true
-            }
-
-            "OPPONENT" -> {
-                false
-            }
-
-            "BOTH" -> {
-                null
-            }
-
-            else -> {
-                null
-            }
-        }
-    }
-    private fun getOrderRadioResult(selectedOption : String) : Boolean {
-        return when(selectedOption){
-            "Date: Newest to oldest" -> {
-                true
-            }
-
-            "Date: Oldest to newest" -> {
-                false
-            }
-
-            else -> {
-                true
-            }
-        }
-    }
-    private fun filterClick(button : ImageView, expansionZone : View) {
-        // CURRENTLY NOT EXPANDED '+'
-        if (expansionZone.visibility != View.VISIBLE){
-            HelperFunctions.expandView(expansionZone)
-            HelperFunctions.rotate45clockwise(button)
-        }
-        // CURRENTLY EXPANDED 'X'
-        else {
-            HelperFunctions.collapseView(expansionZone)
-            HelperFunctions.rotate45anticlockwise(button)
         }
     }
 
