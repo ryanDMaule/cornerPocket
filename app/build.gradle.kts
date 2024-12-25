651b15e1bebf9ba88c50870b3165db1efb7cf159
ryanDMaule <davidmaule98@gmail.com>
2024-12-25 21:56:57 +0000

    Ad update
    - Was using test admbob credentials, now been updated to use real version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mauleCo.cornerpocket"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
